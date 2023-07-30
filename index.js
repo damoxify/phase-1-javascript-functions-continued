90ae7b09c552d4845d227e593eea877a2a1cafe2
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-30 13:01:53 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+  }
+
+  function wrapAdjective(result = "*") {
+    return function (adjective = "a hard worker") {
+      return `You are ${result}${adjective}${result}!`;
+    };
+  }
+
+  function wrapAdjective(result = "||") {
+    return function (adjective = "a dedicated programmer") {
+      return `You are ${result}${adjective}${result}!`;
+    };
+  }
